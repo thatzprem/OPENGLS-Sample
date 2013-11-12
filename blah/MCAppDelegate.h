9aa2f6449b37617defbf53a87452c46b7f0a8e0e
Prem kumar <Prem@Prems-MacBook-Pro-2.local>
2013-11-12 12:15:43 +0530

    Uploading sample application

---
+++ blah/MCAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  MCAppDelegate.h
+//  blah
+//
+//  Created by Prem Kumar on 9/6/11.
+//  Copyright (c) 2011 FreakApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MCViewController;
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MCViewController *viewController;
+
+@end
---
+++ blah/MCViewController.h
@@ -0,0 +1,14 @@
+//
+//  MCViewController.h
+//  blah
+//
+//  Created by Prem Kumar on 9/6/11.
+//  Copyright (c) 2011 FreakApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface MCViewController : GLKViewController
+
+@end
